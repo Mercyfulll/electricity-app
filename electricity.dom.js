ffe4aa22159e015db6c63a1076b2ec79e8869201
Mercyfulll <mercyful.buthelezi@gmail.com>
2023-05-30 13:14:18 +0200

    first commit

--- electricity.dom.js
+++ electricity.dom.js
@@ -1,9 +1,40 @@
 // DOM element(s) references
+const buyButton = document.querySelector(".topupNow");
+const unitsUseable = document.querySelector(".unitsAvailable");
+const unitsBought = document.querySelector(".totalUnits");
+const totalAmountSpent = document.querySelector(".totalAmount");
+const advanceTaken = document.querySelector(".advanceTaken");
+const useButton = document.querySelector(".useNow")
+const radioBtn = document.querySelector("input[name='buyElectricity']:checked");
+const radioButton = document.querySelector("input[name='useElectricity']:checked")
+
 
 
 
 // Factory Function instance 
 const electricity =  Electricity();
 
+unitsBought.innerHTML = localStorage.getItem("theUnits") || 0
+
 
 // DOM events here 
+buyButton.addEventListener("click",function(){
+    var radioBtn = document.querySelector("input[name='buyElectricity']:checked");
+    if(radioBtn){
+        electricity.topUpElectricity(radioBtn.value);
+        localStorage.setItem("theUnits", electricity.getUnitsAvailable());
+        var theUnitsBought = parseInt(localStorage.getItem("theUnits"));
+        unitsBought.innerHTML = theUnitsBought;
+    }
+})
+
+useButton.addEventListener("click",function(){
+    var radioButton = document.querySelector("input[name='useElectricity']:checked");
+    if(radioButton){
+        electricity.useAppliance(radioButton.value)
+        localStorage.setItem("unitsAvailable",electricity.getUnitsAvailable());
+        unitsUseable.innerHTML = localStorage.getItem("unitsAvailable");
+        
+    }    
+
+})
--- electricity.js
+++ electricity.js
@@ -1,5 +1,8 @@
 function Electricity() {
 
+    let unitsAvailable = 0
+    let totalUnits = 0
+    let totalAmount = 0
     // do we want to go with this or array? 
     let appliances = {
         'Stove': 10, 
@@ -9,7 +12,18 @@ function Electricity() {
     };
 
     function topUpElectricity(amount) {
-
+        if(amount == 50){
+            unitsAvailable += 35
+        }
+        else if(amount == 20){
+            unitsAvailable = unitsAvailable + 14
+        }
+        else if(amount == 10){
+            unitsAvailable = unitsAvailable + 7
+        }
+        else if(amount === 'advance'){
+            unitsAvailable += 21
+        }        
 
     }
 
@@ -22,16 +36,42 @@ function Electricity() {
     * other wise return false and do nothing.
     */
     function useAppliance(appliance) {
-        
+        if(appliance == "TV" && unitsAvailable >= 3){
+            unitsAvailable -= 3
+            return true
+        }
+        if(appliance === "Kettle" && unitsAvailable >= 5){
+            unitsAvailable -= 5
+            return true
+        }
+        if(appliance === "stove" && unitsAvailable >= 10){
+            unitsAvailable -= 10
+            return true
+        }
+        if(appliance === "Fridge" && unitsAvailable >= 13){
+            unitsAvailable -= 13
+            return true
+        }
+        else{
+            return false
+        }
     }
 
     function advanceTaken() {
+        var isUsed = false;
+        if(!isUsed){
+            isUsed = true;
+            unitsAvailable += 21
+        }else{
+            return false;
+        }
     }
 
     function totalAmountSpent() {
     }
 
     function totalUnitsBought(){
+            
     }
 
     return {
